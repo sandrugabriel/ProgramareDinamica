c7ae37b917dd3f0165fdec0e4483652d50945599
sandrugabriel <sandru11.gabriel@gmail.com>
2024-03-09 11:41:23 +0200

    done

---
+++ Tema3/Solutii.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema3
+{
+    public class Solutii
+    {
+        Functii functii = new Functii();
+
+        /*Given an integer n, return a list of all possible full binary trees with n nodes. Each node of each tree in the answer must have Node.val == 0.
+Each element of the answer is the root node of one possible tree. You may return the final list of trees in any order.
+A full binary tree is a binary tree where each node has exactly 0 or 2 children.
+Example 1:
+Input: n = 7
+Output: [[0,0,0,null,null,0,0,null,null,0,0],[0,0,0,null,null,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,null,null,null,null,0,0],[0,0,0,0,0,null,null,0,0]]
+Example 2:
+
+Input: n = 3
+Output: [[0,0,0]]
+*/
+        public void solutia1()
+        {
+            foreach (var item in functii.AllPossibleFBT(3))
+            {
+                    Console.WriteLine(item.val.ToString());
+            }
+        }
+
+        /*You are given a 0-indexed m x n integer matrix grid consisting of distinct integers from 0 to m * n - 1. 
+ * You can move in this matrix from a cell to any other cell in the next row. That is, if you are in cell 
+ * (x, y) such that x < m - 1, you can move to any of the cells (x + 1, 0), (x + 1, 1), ..., (x + 1, n - 1).
+ * Note that it is not possible to move from cells in the last row.
+Each possible move has a cost given by a 0-indexed 2D array moveCost of size (m * n) x n, where moveCost[i][j] is 
+the cost of moving from a cell with value i to a cell in column j of the next row. The cost of moving from 
+cells in the last row of grid can be ignored.
+The cost of a path in grid is the sum of all values of cells visited plus the sum of costs of all the moves made. Return 
+the minimum cost of a path that starts from any cell in the first row and ends at any cell in the last row.
+Example 1:
+
+
+Input: grid = [[5,3],[4,0],[2,1]], moveCost = [[9,8],[1,5],[10,12],[18,6],[2,4],[14,3]]
+Output: 17
+Explanation: The path with the minimum possible cost is the path 5 -> 0 -> 1.
+- The sum of the values of cells visited is 5 + 0 + 1 = 6.
+- The cost of moving from 5 to 0 is 3.
+- The cost of moving from 0 to 1 is 8.
+So the total cost of the path is 6 + 3 + 8 = 17.
+Example 2:
+
+Input: grid = [[5,1,2],[4,0,3]], moveCost = [[12,10,15],[20,23,8],[21,7,1],[8,1,13],[9,10,25],[5,3,2]]
+Output: 6
+Explanation: The path with the minimum possible cost is the path 2 -> 3.
+- The sum of the values of cells visited is 2 + 3 = 5.
+- The cost of moving from 2 to 3 is 1.
+So the total cost of this path is 5 + 1 = 6.
+*/
+        public void solutia2()
+        {
+            int[][] grid = [[5, 3], [4, 0], [2, 1]], moveCost = [[9, 8], [1, 5], [10, 12], [18, 6], [2, 4], [14, 3]];
+            Console.WriteLine(functii.MinPathCost(grid,moveCost));
+        }
+
+        /*You are given two integer arrays nums1 and nums2. We write the integers of nums1 and nums2 (in the order they are given) on two separate horizontal lines.
+
+We may draw connecting lines: a straight line connecting two numbers nums1[i] and nums2[j] such that:
+
+nums1[i] == nums2[j], and
+the line we draw does not intersect any other connecting (non-horizontal) line.
+Note that a connecting line cannot intersect even at the endpoints (i.e., each number can only belong to one connecting line).
+
+Return the maximum number of connecting lines we can draw in this way.
+
+ 
+
+Example 1:
+
+
+Input: nums1 = [1,4,2], nums2 = [1,2,4]
+Output: 2
+Explanation: We can draw 2 uncrossed lines as in the diagram.
+We cannot draw 3 uncrossed lines, because the line from nums1[1] = 4 to nums2[2] = 4 will intersect the line from nums1[2]=2 to nums2[1]=2.
+Example 2:
+
+Input: nums1 = [2,5,1,2,5], nums2 = [10,5,2,1,5,2]
+Output: 3
+Example 3:
+
+Input: nums1 = [1,3,7,1,7,5], nums2 = [1,9,2,5,1]
+Output: 2*/
+        public void solutia3() {
+            int[] nums1 = [1, 4, 2], nums2 = [1, 2, 4];
+            Console.WriteLine(functii.MaxUncrossedLines(nums1,nums2));
+
+        }
+
+        /*There is a survey that consists of n questions where each question's answer is either 0 (no) or 1 (yes).
+
+The survey was given to m students numbered from 0 to m - 1 and m mentors numbered from 0 to m - 1. The answers of the students are represented by a 2D integer array students where students[i] is an integer array that contains the answers of the ith student (0-indexed). The answers of the mentors are represented by a 2D integer array mentors where mentors[j] is an integer array that contains the answers of the jth mentor (0-indexed).
+
+Each student will be assigned to one mentor, and each mentor will have one student assigned to them. The compatibility score of a student-mentor pair is the number of answers that are the same for both the student and the mentor.
+
+For example, if the student's answers were [1, 0, 1] and the mentor's answers were [0, 0, 1], then their compatibility score is 2 because only the second and the third answers are the same.
+You are tasked with finding the optimal student-mentor pairings to maximize the sum of the compatibility scores.
+
+Given students and mentors, return the maximum compatibility score sum that can be achieved.
+
+ 
+
+Example 1:
+
+Input: students = [[1,1,0],[1,0,1],[0,0,1]], mentors = [[1,0,0],[0,0,1],[1,1,0]]
+Output: 8
+Explanation: We assign students to mentors in the following way:
+- student 0 to mentor 2 with a compatibility score of 3.
+- student 1 to mentor 0 with a compatibility score of 2.
+- student 2 to mentor 1 with a compatibility score of 3.
+The compatibility score sum is 3 + 2 + 3 = 8.
+Example 2:
+
+Input: students = [[0,0],[0,0],[0,0]], mentors = [[1,1],[1,1],[1,1]]
+Output: 0
+Explanation: The compatibility score of any student-mentor pair is 0.*/
+        public void solutia4()
+        {
+            int[][] students = [[1, 1, 0], [1, 0, 1], [0, 0, 1]], mentors = [[1, 0, 0], [0, 0, 1], [1, 1, 0]];
+            Console.WriteLine(functii.MaxCompatibilitySum(students,mentors));
+        }
+
+        /*You are given an array of words where each word consists of lowercase English letters.
+
+wordA is a predecessor of wordB if and only if we can insert exactly one letter anywhere in wordA without changing the order of the other characters to make it equal to wordB.
+
+For example, "abc" is a predecessor of "abac", while "cba" is not a predecessor of "bcad".
+A word chain is a sequence of words [word1, word2, ..., wordk] with k >= 1, where word1 is a predecessor of word2, word2 is a predecessor of word3, and so on. A single word is trivially a word chain with k == 1.
+
+Return the length of the longest possible word chain with words chosen from the given list of words.
+
+ 
+
+Example 1:
+
+Input: words = ["a","b","ba","bca","bda","bdca"]
+Output: 4
+Explanation: One of the longest word chains is ["a","ba","bda","bdca"].
+Example 2:
+
+Input: words = ["xbc","pcxbcf","xb","cxbc","pcxbc"]
+Output: 5
+Explanation: All the words can be put in a word chain ["xb", "xbc", "cxbc", "pcxbc", "pcxbcf"].
+Example 3:
+
+Input: words = ["abcd","dbqca"]
+Output: 1
+Explanation: The trivial word chain ["abcd"] is one of the longest word chains.
+["abcd","dbqca"] is not a valid word chain because the ordering of the letters is changed.
+ */
+        public void solutia5()
+        {
+            string[] words = ["a", "b", "ba", "bca", "bda", "bdca"];
+            Console.Write(functii.LongestStrChain(words));
+        }
+
+        /*Given an integer n, return the number of structurally unique BST's (binary search trees) which has exactly n nodes of unique values from 1 to n.
+
+
+
+ Example 1:
+
+
+ Input: n = 3
+ Output: 5
+ Example 2:
+
+ Input: n = 1
+ Output: 1*/
+        public void solutia6()
+        {
+            Console.WriteLine(functii.NumTrees(5));
+        }
+
+        /*You have n dice, and each dice has k faces numbered from 1 to k.
+
+Given three integers n, k, and target, return the number of possible ways (out of the kn total ways) to roll the dice, so the sum of the face-up numbers equals target. Since the answer may be too large, return it modulo 109 + 7.
+
+ 
+
+Example 1:
+
+Input: n = 1, k = 6, target = 3
+Output: 1
+Explanation: You throw one die with 6 faces.
+There is only one way to get a sum of 3.
+Example 2:
+
+Input: n = 2, k = 6, target = 7
+Output: 6
+Explanation: You throw two dice, each with 6 faces.
+There are 6 ways to get a sum of 7: 1+6, 2+5, 3+4, 4+3, 5+2, 6+1.
+Example 3:
+
+Input: n = 30, k = 30, target = 500
+Output: 222616187
+Explanation: The answer must be returned modulo 109 + 7.
+ */
+        public void solutia7()
+        {
+            Console.WriteLine(functii.NumRollsToTarget(30,30,500));
+
+        }
+
+        /*Given an integer n, break it into the sum of k positive integers, where k >= 2, and maximize the product of those integers.
+
+Return the maximum product you can get.
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: 2 = 1 + 1, 1 × 1 = 1.
+Example 2:
+
+Input: n = 10
+Output: 36
+Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
+ */
+        public void solutia8()
+        {
+            Console.WriteLine(functii.IntegerBreak(2));
+        }
+
+        /*You are given an array of n pairs pairs where pairs[i] = [lefti, righti] and lefti < righti.
+
+A pair p2 = [c, d] follows a pair p1 = [a, b] if b < c. A chain of pairs can be formed in this fashion.
+
+Return the length longest chain which can be formed.
+
+You do not need to use up all the given intervals. You can select pairs in any order.
+
+ 
+
+Example 1:
+
+Input: pairs = [[1,2],[2,3],[3,4]]
+Output: 2
+Explanation: The longest chain is [1,2] -> [3,4].
+Example 2:
+
+Input: pairs = [[1,2],[7,8],[4,5]]
+Output: 3
+Explanation: The longest chain is [1,2] -> [4,5] -> [7,8].
+ */
+        public void solutia9()
+        {
+            Console.WriteLine(functii.FindLongestChain([[1, 2], [2, 3], [3, 4]]));
+        }
+
+        /*You are given an integer array nums. The absolute sum of a subarray [numsl, numsl+1, ..., numsr-1, numsr] is abs(numsl + numsl+1 + ... + numsr-1 + numsr).
+
+Return the maximum absolute sum of any (possibly empty) subarray of nums.
+
+Note that abs(x) is defined as follows:
+
+If x is a negative integer, then abs(x) = -x.
+If x is a non-negative integer, then abs(x) = x.
+ 
+
+Example 1:
+
+Input: nums = [1,-3,2,3,-4]
+Output: 5
+Explanation: The subarray [2,3] has absolute sum = abs(2+3) = abs(5) = 5.
+Example 2:
+
+Input: nums = [2,-5,1,-4,3,-2]
+Output: 8
+Explanation: The subarray [-5,1,-4] has absolute sum = abs(-5+1-4) = abs(-8) = 8.
+ */
+        public void solutia10()
+        {
+            Console.WriteLine(functii.MaxAbsoluteSum([1, -3, 2, 3, -4]));
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
